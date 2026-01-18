d4de024707fbad5eb67d1f6e2e1f233e9bad48d7
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-18 15:14:14 +0900

    feat: ga4 버튼이랑 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import Button from "./shared/components/button/button";
 import ThemeProvider from "./shared/components/theme-provider";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
+  const handleButtonClick = () => {
+    ReactGA.event({
+      category: "Conversion",
+      action: "Click_CTA_Button",
+      label: "start service",
+    });
+  };
   return (
     <ThemeProvider>
       <p>바닐라 익스트랙 스타일 적용</p>
-      <Button>primary button component</Button>
+      <Button onClick={handleButtonClick}>primary button component</Button>
       <Button variant="border">border button component</Button>
-      <Button variant="gray_fill" size="sm" >gray fill button</Button>
-      <Button variant="gray_fill" size="sm" disabled>gray fill disabled button</Button>
+      <Button variant="gray_fill" size="sm">
+        gray fill button
+      </Button>
+      <Button variant="gray_fill" size="sm" disabled>
+        gray fill disabled button
+      </Button>
     </ThemeProvider>
   );
 }
