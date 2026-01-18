5494abc707890b85fb49fcf1d29d4984cb07c71d
hummingbbird <pop9814@kookmin.ac.kr>
2026-01-18 14:57:22 +0900

    feat: ga4 설치 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './shared/styles/reset.css.ts';
 import './shared/styles/global.css.ts';
 import App from './App.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
